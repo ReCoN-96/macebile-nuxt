e53b634d2eb25c9900afbd3fa796e8e1060784f8
recon-96 <osf@hotmail.fr>
2023-05-02 19:14:02 +0200

    try to fix svg

--- components/Practices.vue
+++ components/Practices.vue
@@ -42,11 +42,11 @@ import PracticesForm from '~/components/PracticesForm.vue'
       </div>
     </div>
     <PracticesForm class="hidden lg:block" />
-    <div class="custom-shape-divider-bottom-1681142732">
+    <!-- <div class="custom-shape-divider-bottom-1681142732">
       <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill" />
       </svg>
-    </div>
+    </div> -->
   </div>
 </template>
 
